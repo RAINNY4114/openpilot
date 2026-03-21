b8c1a74d26e7a070bf50c741df51b46d83653c3d
zhqhx <18267890110@163.com>
2026-03-21 11:03:51 +0800

    修改右上角图标缩小到320x320

--- selfdrive/ui/onroad/hud_renderer.py
+++ selfdrive/ui/onroad/hud_renderer.py
@@ -28,7 +28,7 @@ class UIConfig:
   set_speed_width_metric: int = 200
   set_speed_width_imperial: int = 172
   set_speed_height: int = 204
-  wheel_icon_size: int = 360
+  wheel_icon_size: int = 320
 
 
 @dataclass(frozen=True)
