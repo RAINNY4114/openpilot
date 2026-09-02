03559d0fe2abb9233bdc42a004af03699bc55a38
RAINNY4114 <152569703+RAINNY4114@users.noreply.github.com>
2026-09-02 12:04:00 +0800

    Update carstate.py

--- opendbc_repo/opendbc/car/ford/carstate.py
+++ opendbc_repo/opendbc/car/ford/carstate.py
@@ -72,16 +72,23 @@ class CarState(CarStateBase, MadsCarState):
 
     # gear
     if self.CP.transmissionType == TransmissionType.automatic:
-       if (cp.vl["TransGearData"]["GearLvrPos_D_Actl"] in (3, 4, 5)):
-       ret.gearShifter = GearShifter.drive
-       elif (cp.vl["TransGearData"]["GearLvrPos_D_Actl"] == 1):
-       ret.gearShifter = GearShifter.reverse
+      gear_lvr_pos = cp.vl["TransGearData"]["GearLvrPos_D_Actl"]
+
+      if gear_lvr_pos in (3, 4, 5):
+        ret.gearShifter = GearShifter.drive
+      elif gear_lvr_pos == 1:
+        ret.gearShifter = GearShifter.reverse
+
     elif self.CP.transmissionType == TransmissionType.manual:
-      ret.clutchPressed = cp.vl["Engine_Clutch_Data"]["CluPdlPos_Pc_Meas"] > 0
+      ret.clutchPressed = (
+        cp.vl["Engine_Clutch_Data"]["CluPdlPos_Pc_Meas"] > 0
+      )
+
       if bool(cp.vl["BCM_Lamp_Stat_FD1"]["RvrseLghtOn_B_Stat"]):
         ret.gearShifter = GearShifter.reverse
       else:
         ret.gearShifter = GearShifter.drive
+
 
     # safety
     ret.stockFcw = bool(cp_cam.vl["ACCDATA_3"]["FcwVisblWarn_B_Rq"])
